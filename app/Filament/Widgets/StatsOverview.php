fd127f8b61f4db875018c3cc582094ac2c43b912
rafiqa944 <rafiqanurc@gmail.com>
2025-05-23 20:18:41 +0800

    first commit

---
+++ app/Filament/Widgets/StatsOverview.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use App\Models\Transaction;
+
+class StatsOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        $pemasukan = Transaction::incomes()->get()->sum('amount');
+        $pengeluaran = Transaction::expenses()->get()->sum('amount');
+        return [
+            Stat::make('Total Pemasukan', $pemasukan),
+            Stat::make('Total Pengeluaran', $pengeluaran),
+            Stat::make('Selisih', $pemasukan - $pengeluaran),
+        ];
+    }
+}
