da47552f94fb7c6a24cc01260fae6b6b8f20bb53
rafiqa944 <rafiqanurc@gmail.com>
2025-05-24 12:46:47 +0800

    mengubah icon pada halaman transaksi

--- app/Filament/Resources/TransactionResource.php
+++ app/Filament/Resources/TransactionResource.php
@@ -19,7 +19,7 @@ class TransactionResource extends Resource
 {
     protected static ?string $model = Transaction::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-arrows-right-left';
 
     public static function form(Form $form): Form
     {
